85476528e0c0af4445300f397d5717186285a982
Petr Galaburda <theSonOfMan@MacBook-Pro-Petr.local>
2018-09-24 14:31:50 +0300

    functional version

--- src/wainterface/main/views.py
+++ src/wainterface/main/views.py
@@ -1,8 +1,23 @@
+import json
+import requests
+
 from django.shortcuts import render
+
 
 def index_view(request, *args, **kwargs):
     return render(request, "index.html", {})
 
 def result_view(request, *args, **kwargs):
-    return render(request, "result.html", {})
+    print(request.POST)
+    r = requests.post("http://api.wolframalpha.com/v2/query", data={
+        'appid':'VU8KTY-XE5EKU7L24',
+        'input':request.POST['input'],
+        'output':'json'})
+    info = []
+    query = r.json()['queryresult']
+    for pod_number in range(0,query['numpods']):
+        info.append({'title':query['pods'][pod_number]['title'],
+         'info':[{'src':x['img']['src'], 'alt':x['img']['alt']} for x in query['pods'][pod_number]['subpods']]})
+    print(info)
+    return render(request, "result.html", {'info':info})
 # Create your views here.
