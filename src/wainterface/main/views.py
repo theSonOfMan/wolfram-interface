b01b6fd681c38caeee76d764ae4603126ece8191
Petr Galaburda <theSonOfMan@MacBook-Pro-Petr.local>
2018-09-26 01:57:32 +0300

    added radioboxes for additional parameters and raw output

--- src/wainterface/main/views.py
+++ src/wainterface/main/views.py
@@ -8,16 +8,21 @@ def index_view(request, *args, **kwargs):
     return render(request, "index.html", {})
 
 def result_view(request, *args, **kwargs):
+    info = []
     print(request.POST)
     r = requests.post("http://api.wolframalpha.com/v2/query", data={
         'appid':'VU8KTY-XE5EKU7L24',
         'input':request.POST['input'],
+        'units':request.POST['metricRadio'],
         'output':'json'})
-    info = []
-    query = r.json()['queryresult']
-    for pod_number in range(0,query['numpods']):
-        info.append({'title':query['pods'][pod_number]['title'],
-         'info':[{'src':x['img']['src'], 'alt':x['img']['alt']} for x in query['pods'][pod_number]['subpods']]})
-    print(info)
+    if request.POST['rawRadio'] == 'true':
+        info.append({'title':"Raw output", 'output':json.dumps(r.json(), sort_keys=True, indent=4)})
+    else:
+        # print(r.json())
+        query = r.json()['queryresult']
+        for pod_number in range(0,query['numpods']):
+            info.append({'title':query['pods'][pod_number]['title'],
+             'info':[{'src':x['img']['src'], 'alt':x['img']['alt']} for x in query['pods'][pod_number]['subpods']]})
+        # print(info)
     return render(request, "result.html", {'info':info})
 # Create your views here.
