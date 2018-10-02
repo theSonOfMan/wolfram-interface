d258c7d45ab2a312268e3b4b33a37c17eba546d0
Petr Galaburda <theSonOfMan@MacBook-Pro-Petr.local>
2018-10-03 02:00:52 +0300

    added step-by-step solutions, improved result template

--- src/wainterface/main/views.py
+++ src/wainterface/main/views.py
@@ -9,12 +9,15 @@ def index_view(request, *args, **kwargs):
 
 def result_view(request, *args, **kwargs):
     info = []
-    print(request.POST)
+    # print(request.POST)
     r = requests.post("http://api.wolframalpha.com/v2/query", data={
         'appid':'VU8KTY-XE5EKU7L24',
         'input':request.POST['input'],
         'units':request.POST['metricRadio'],
+        'podstate':'Step-by-step solution',
+        'format':'image',
         'output':'json'})
+    # print(r.json())
     if request.POST['rawRadio'] == 'true':
         info.append({'title':"Raw output", 'output':json.dumps(r.json(), sort_keys=True, indent=4)})
     else:
@@ -22,7 +25,7 @@ def result_view(request, *args, **kwargs):
         query = r.json()['queryresult']
         for pod_number in range(0,query['numpods']):
             info.append({'title':query['pods'][pod_number]['title'],
-             'info':[{'src':x['img']['src'], 'alt':x['img']['alt']} for x in query['pods'][pod_number]['subpods']]})
-        # print(info)
+             'info':[{'title':x['title'],'src':x['img']['src'], 'alt':x['img']['alt']} for x in query['pods'][pod_number]['subpods']]})
+        print(info)
     return render(request, "result.html", {'info':info})
 # Create your views here.
