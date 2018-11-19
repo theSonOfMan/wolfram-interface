11219c39e5bb716cc9a1006e46453aab7b2ffd43
theSonOfMan <petr.galaburda@gmail.com>
2018-11-19 22:33:15 +0300

    flask init

--- src/test/wolfram_requests.py
+++ src/test/wolfram_requests.py
@@ -9,4 +9,10 @@ r = requests.post("http://api.wolframalpha.com/v2/query", data={
     'appid':'VU8KTY-XE5EKU7L24',
     'input':'65 million people',
     'output':'json'})
-print(json.dumps(r.json()))
+info = []
+query = r.json()['queryresult']
+for pod_number in range(0,query['numpods']):
+    info.append({'title':query['pods'][pod_number]['title'],
+     'info':[{'src':x['img']['src'], 'alt':x['img']['alt']} for x in query['pods'][pod_number]['subpods']]})
+print(json.dumps(info))
+# print(json.dumps(r.json()))
